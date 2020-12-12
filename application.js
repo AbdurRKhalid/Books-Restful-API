66598a7cf572530c6f54b123fb2924897b804dc3
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2020-12-12 10:02:31 +0500

    Added the Processing of JSON using Body-Parser.

--- application.js
+++ application.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
-
+const bodyParser = require("body-parser");
 const application = express();
 
 application.use(morgan("dev"));
+application.use(bodyParser.urlencoded({ urlencoded: true }));
+application.use(bodyParser.json());
 
 module.exports = application;
