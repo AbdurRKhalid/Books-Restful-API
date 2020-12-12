655e59de357382539adf1aed679cfd7c21f2c5f2
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2020-12-12 10:13:39 +0500

    Allowed the Application to Use the URL Encoding. Allowed the Application to Use the JSON Data Type. Setted the Headers for the CORS.

--- application.js
+++ application.js
@@ -4,7 +4,18 @@ const bodyParser = require("body-parser");
 const application = express();
 
 application.use(morgan("dev"));
-application.use(bodyParser.urlencoded({ urlencoded: true }));
-application.use(bodyParser.json());
+application.use(express.urlencoded({ extended: true }));
+application.use(express.json());
+
+// Defining the Structure of Headers and Things If An Empty Request Comes.
+application.use(function handlingEmptyRequest(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT POST PATCH GET DELETE");
+    res.status(200).json({});
+  }
+  next();
+});
 
 module.exports = application;
